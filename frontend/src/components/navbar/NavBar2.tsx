d7f1bf0569181c76f5ca12022e2a87e5c281c191
Deval Parikh <devalpp@gmail.com>
2021-12-09 22:51:04 -0500

    Buttons under menu workaround

--- frontend/src/components/navbar/NavBar2.tsx
+++ frontend/src/components/navbar/NavBar2.tsx
@@ -1,54 +1,63 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { useRef } from "react";
 import { motion, useCycle } from "framer-motion";
 import { useDimensions } from "./use-dimensions";
 import { MenuToggle } from "./MenuToggle";
 import { Navigation } from "./Navigation";
-import './NavBar2.css';
-
+import "./NavBar2.css";
 
 const sidebar = {
-    open: (height = 1000) => ({
-        clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-        transition: {
-            type: "spring",
-            stiffness: 20,
-            restDelta: 2
-        }
-    }),
-    closed: {
-        clipPath: "circle(30px at 40px 41px)",
-        transition: {
-            delay: 0.06,
-            type: "spring",
-            stiffness: 400,
-            damping: 40,
-        },
-    }
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 41px)",
+    transition: {
+      delay: 0.06,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
 };
 
 export const NavBar2 = () => {
-    const [isOpen, toggleOpen] = useCycle(false, true);
-    const containerRef = useRef(null);
-    // const { height } = useDimensions(containerRef);
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const [isOpenStyle, setIsOpenStyle] = useState(false);
+  const containerRef = useRef(null);
+  // const { height } = useDimensions(containerRef);
 
-    // Force disable navigation buttons when closed
-    let menuStyleClass = isOpen ? "" : "nav-disabled-buttons";
+  // Force disable navigation buttons when closed
+  let menuStyleClass = isOpenStyle || isOpen ? "" : "nav-disabled-buttons";
 
-    return (
-        <motion.nav
-            className={menuStyleClass}
-            initial={false}
-            animate={isOpen ? "open" : "closed"}
-            // custom={height}
-            ref={containerRef}
-        >
-            <motion.div className="nav-background nav-disabled" variants={sidebar} />
-            <Navigation />
-            <MenuToggle toggle={() => toggleOpen()} />
-        </motion.nav>
-    );
+  useEffect(() => {
+    const lock = isOpen ? 0 : 1000;
+
+    setTimeout(() => {
+      if (isOpenStyle !== isOpen) {
+        setIsOpenStyle(isOpen);
+      }
+    }, lock);
+  }, [isOpen]);
+
+  return (
+    <motion.nav
+      className={menuStyleClass}
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      // custom={height}
+      ref={containerRef}
+    >
+      <motion.div className="nav-background nav-disabled" variants={sidebar} />
+      <Navigation />
+      <MenuToggle toggle={() => toggleOpen()} />
+    </motion.nav>
+  );
 };
 
 export default NavBar2;
-
