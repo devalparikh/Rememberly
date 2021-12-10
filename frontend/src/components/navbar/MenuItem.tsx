67adb68c28d3e7088b5f1191da45c2d3f9f16e5b
Deval Parikh <devalpp@gmail.com>
2021-12-09 22:51:16 -0500

    Cleanup

--- frontend/src/components/navbar/MenuItem.tsx
+++ frontend/src/components/navbar/MenuItem.tsx
@@ -3,46 +3,47 @@ import { motion } from "framer-motion";
 import { link } from "fs";
 
 const variants = {
-    open: {
-        y: 0,
-        opacity: 1,
-        transition: {
-            y: { stiffness: 1000, velocity: -100 }
-        }
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
     },
-    closed: {
-        y: 50,
-        opacity: 0,
-        transition: {
-            y: { stiffness: 1000 }
-        },
-        zIndex: -2
-    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+    zIndex: -2,
+  },
 };
 
 const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
 
-let navLinks = ["Home", "About", "Login", "Register"]
+let navLinks = ["Home", "About", "Login", "Register"];
 
-if(localStorage.usertoken) {
-    navLinks = ["Home", "Journal", "Profile", "Logout"]
+if (localStorage.usertoken) {
+  navLinks = ["Home", "Journal", "Profile", "Logout"];
 }
 
-
-export const MenuItem = ({ i }: { i: any}) => {
-    // const IconStyle = { border: `2px solid ${colors[i]}` };
-    const TextStyle = { color: `${colors[i]}`, fontSize: 50, fontWeight: 800 };
-    return (
-        <motion.li
-            className="nav-li"
-            variants={variants}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.95 }}
-        >
-            {/* <div className="icon-placeholder" style={IconStyle} /> */}
-            <div className="text-placeholder">
-                <a style={TextStyle} href={"/"+String(navLinks[i]).toLowerCase()}>{navLinks[i]}</a>
-            </div>
-        </motion.li>
-    );
+export const MenuItem = ({ i }: { i: any }) => {
+  // const IconStyle = { border: `2px solid ${colors[i]}` };
+  const TextStyle = { color: `${colors[i]}`, fontSize: 50, fontWeight: 800 };
+  return (
+    <motion.li
+      className="nav-li"
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      {/* <div className="icon-placeholder" style={IconStyle} /> */}
+      <div className="text-placeholder">
+        <a style={TextStyle} href={"/" + String(navLinks[i]).toLowerCase()}>
+          {navLinks[i]}
+        </a>
+      </div>
+    </motion.li>
+  );
 };
